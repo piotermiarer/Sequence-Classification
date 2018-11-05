ea0f7b82b1421a8abfd1002cc4b05891a4642856
berezowskij <jakub.berezowski@snpgroup.com>
2018-11-05 10:38:25 +0100

    Add svm classifier with usage example Only mock, hand-written data :/

---
+++ sequence-classification/jupiter_shared/svm_classifier.py
@@ -0,0 +1,35 @@
+from sequence_classifier import SequenceClassifier
+from sequence_transformer import SequenceTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
+# import numpy as np
+from sklearn.svm import SVC
+
+
+class SVMClassifier(SequenceClassifier):
+    def __init__(self, name='SVM'):
+        super(SVMClassifier, self).__init__(name)
+        self.model = SVC(gamma='auto')
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def get_transformer(self):
+        return SVMTransformer()
+
+
+class SVMTransformer(SequenceTransformer):
+    def __init__(self):
+        self.vectorizer = CountVectorizer()
+
+    def transform(self, raw_data):
+        # count vectorizer
+        # X = self.vectorizer.fit_transform(raw_data)
+        # return X.toarray()
+
+        ## NOT WORKING RIGHT NOW
+        return raw_data
