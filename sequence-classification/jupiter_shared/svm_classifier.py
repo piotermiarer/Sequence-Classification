59bea17447a3968bfb96ecd018d6988bbfdf8bd3
berezowskij <jakub.berezowski@snpgroup.com>
2018-11-12 21:45:19 +0100

    SVM and NN classifier merge

--- sequence-classification/jupiter_shared/svm_classifier.py
+++ sequence-classification/jupiter_shared/svm_classifier.py
@@ -1,15 +1,14 @@
 from sequence_classifier import SequenceClassifier
 from sequence_transformer import SequenceTransformer
 from sklearn.feature_extraction.text import CountVectorizer
-import numpy as np
 
 from sklearn.svm import SVC
-
+from sklearn.svm import LinearSVC
 
 class SVMClassifier(SequenceClassifier):
     def __init__(self, name='SVM'):
         super(SVMClassifier, self).__init__(name)
-        self.model = SVC(gamma='auto')
+        self.model = LinearSVC(random_state=0, tol=1e-5)
 
     def fit(self, X_train, y_train):
         self.model.fit(X_train, y_train)
@@ -26,7 +25,13 @@ class SVMTransformer(SequenceTransformer):
         self.vectorizer = CountVectorizer()
 
     def transform(self, raw_data):
-        # count vectorizer
-        X = self.vectorizer.fit_transform(raw_data)
-        return X.toarray()
+        strings_from_list = [' '.join([str(x) for x in data]) for data in raw_data]
+        self.vectorizer.fit_transform(strings_from_list)
+        return self.vectorizer.fit_transform(strings_from_list).toarray()
+
+    def transform_to_predict(self, raw_data):
+        strings_from_list = [' '.join([str(x) for x in data]) for data in raw_data]
+        return self.vectorizer.transform(strings_from_list).toarray()
+
+
 
